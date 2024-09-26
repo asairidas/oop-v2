4535ef63a4c229885dbcc24660f0fea6dc5f9e0a
Airidas <airidas.sukvietis@mif.stud.vu.lt>
2024-09-26 21:37:42 +0300

    Padarau, kad eitu ivesti daugiau nei vieno mokinio duomenis

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
 
 using namespace std;
 
-
-struct Mokinys {
+struct Mokinys
+{
     string vardas;
     string pavarde;
     int namu_darbu_kiekis{0};
@@ -13,11 +16,20 @@ struct Mokinys {
     int egzamino_rezultatas{0};
 };
 
-int main()
+// funkcija, kuri skaiciuoja vidurki ir paima mokinio struktura ir grazina vidurki
+double skaiciuoti_vidurki(Mokinys m)
 {
+    double vidurkis = 0;
+    for (int j = 0; j < m.namu_darbu_rezultatai.size(); j++)
+    {
+        vidurkis += m.namu_darbu_rezultatai[j];
+    }
+    return vidurkis / m.namu_darbu_rezultatai.size();
+}
 
+Mokinys nuskaityti_mokinio_duomenis()
+{
     Mokinys m;
-
     cout << "Iveskite varda:" << endl;
     cin >> m.vardas;
     cout << "Ivedete varda " << m.vardas << endl;
@@ -28,34 +40,43 @@ int main()
     cin >> m.namu_darbu_kiekis;
     cout << "Ivedete " << m.namu_darbu_kiekis << endl;
 
-//   for ( <paruosiamieji veiksmai atliekami pries cikla> ; <salyga - ciklas vykdomas tol, kas salyga tenkinama> ; <kiekvieno ciklo pabaigoje vykdomi veiksmai>)
-
-
-
-    for (int i=0; i < m.namu_darbu_kiekis; i++)
+    for (int i = 0; i < m.namu_darbu_kiekis; i++)
     {
-       int pazymys;
-       cout << "Iveskite pazymi nr " << i+1 << endl;
-       cin >> pazymys;
-       m.namu_darbu_rezultatai.push_back(pazymys);
+        int pazymys;
+        cout << "Iveskite pazymi nr " << i + 1 << endl;
+        cin >> pazymys;
+        m.namu_darbu_rezultatai.push_back(pazymys);
+    }
+    return m;
+}
+
+int main()
+{
+
+    int studentu_skaicius(0);
+    cout << "Iveskite studentu skaiciu" << endl;
+    cin >> studentu_skaicius;
+
+    if (studentu_skaicius <= 0)
+    {
+        cerr << "Studentų skaičius turi būti teigiamas sveikasis skaičius." << endl;
+        return 1;
     }
 
-    double vidurkis=0;
-    for (int j = 0; j < m.namu_darbu_rezultatai.size(); j++)
+    vector<Mokinys> mokiniai;
+
+    for (int k = 0; k < studentu_skaicius; ++k)
     {
-        cout << m.namu_darbu_rezultatai[j];
-        vidurkis += m.namu_darbu_rezultatai[j];
-        if (j < (m.namu_darbu_rezultatai.size()-1))
-        {
-            cout << ", ";
-        }
-        else
-        {
-            cout << endl;
-            cout << endl;
-        }
+        Mokinys m = nuskaityti_mokinio_duomenis();
+        mokiniai.push_back(m);
     }
-    cout << "vidurkis:" << vidurkis / m.namu_darbu_rezultatai.size();
+    cout << left << setw(15) << "Vardas" << setw(15) << "Pavarde" << setw(20) << "Galutinis(vid.)" << endl;
+    cout << "------------------------------------------" << endl;
+    for (int i = 0; i < mokiniai.size(); i++)
+    {
+        auto vidurkis = skaiciuoti_vidurki(mokiniai[i]);
+        cout << left << setw(15) << mokiniai[i].vardas << setw(15) << mokiniai[i].pavarde << setw(20) << vidurkis << endl;
+    }
 
     return 0;
 }
