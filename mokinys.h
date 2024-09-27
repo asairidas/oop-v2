3d35a3b582258027232ccd8d6316ecae5f162b15
Airidas <airidas.sukvietis@mif.stud.vu.lt>
2024-09-27 21:01:58 +0300

    iskeliau mokinio struktura i *h failą

---
+++ mokinys.h
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Mokinys
+{
+    string vardas;
+    string pavarde;
+    int namu_darbu_kiekis{0};
+    vector<int> namu_darbu_rezultatai;
+    int egzamino_rezultatas{0};
+};
