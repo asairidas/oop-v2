3adec5c1f4c972e9dfe91ed68f06727143d5a46d
Airidas <airidas.sukvietis@mif.stud.vu.lt>
2024-12-22 15:50:27 +0200

    pridejau abstrakcia klase zmogus

---
+++ zmogus.h
@@ -0,0 +1,21 @@
+#ifndef ZMOGUS_H
+#define ZMOGUS_H
+
+#include <string>
+
+class Zmogus
+{
+protected:
+    std::string vardas_;
+    std::string pavarde_;
+
+public:
+    Zmogus(const std::string &vardas, const std::string &pavarde)
+        : vardas_(vardas), pavarde_(pavarde) {}
+
+    virtual ~Zmogus() = default;
+
+    virtual void spausdink() const = 0; // grynoji virtualioji funkcija
+};
+
+#endif // ZMOGUS_H
